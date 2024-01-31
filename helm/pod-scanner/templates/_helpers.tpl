0b695e39e6a75a3f1a535bd1e02726cd8b1be020
rowland <rowlandr71@gmail.com>
2024-01-31 15:36:38 +0100

    added helm chart for the application and html landing page

---
+++ helm/pod-scanner/templates/_helpers.tpl
@@ -0,0 +1,62 @@
+{{/*
+Expand the name of the chart.
+*/}}
+{{- define "pod-scanner.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
+{{- end }}
+
+{{/*
+Create a default fully qualified app name.
+We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
+If release name contains chart name it will be used as a full name.
+*/}}
+{{- define "pod-scanner.fullname" -}}
+{{- if .Values.fullnameOverride }}
+{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- $name := default .Chart.Name .Values.nameOverride }}
+{{- if contains $name .Release.Name }}
+{{- .Release.Name | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" }}
+{{- end }}
+{{- end }}
+{{- end }}
+
+{{/*
+Create chart name and version as used by the chart label.
+*/}}
+{{- define "pod-scanner.chart" -}}
+{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
+{{- end }}
+
+{{/*
+Common labels
+*/}}
+{{- define "pod-scanner.labels" -}}
+helm.sh/chart: {{ include "pod-scanner.chart" . }}
+{{ include "pod-scanner.selectorLabels" . }}
+{{- if .Chart.AppVersion }}
+app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
+{{- end }}
+app.kubernetes.io/managed-by: {{ .Release.Service }}
+{{- end }}
+
+{{/*
+Selector labels
+*/}}
+{{- define "pod-scanner.selectorLabels" -}}
+app.kubernetes.io/name: {{ include "pod-scanner.name" . }}
+app.kubernetes.io/instance: {{ .Release.Name }}
+{{- end }}
+
+{{/*
+Create the name of the service account to use
+*/}}
+{{- define "pod-scanner.serviceAccountName" -}}
+{{- if .Values.serviceAccount.create }}
+{{- default (include "pod-scanner.fullname" .) .Values.serviceAccount.name }}
+{{- else }}
+{{- default "default" .Values.serviceAccount.name }}
+{{- end }}
+{{- end }}
--- pod_scanner.py
+++ pod_scanner.py
@@ -1,34 +1,40 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, render_template
 from kubernetes import client, config
 
 app = Flask(__name__)
 
-def get_pods_with_status(api_instance, namespace, pod_status):
-    pods = api_instance.list_namespaced_pod(namespace)
+def get_pods_with_status(api_instance, pod_status, pod_namespace=None):
+    pods = api_instance.list_pod_for_all_namespaces(watch=False)
     matching_pods = [pod.metadata.name for pod in pods.items if pod.status.phase == pod_status]
-    return matching_pods
+    
+    if pod_namespace is not None:
+        matching_pods = [pod for pod in matching_pods if pod.metadata.namespace == pod_namespace]
+    
+    matching_pod_namespaces = {pod.metadata.name: pod.metadata.namespace for pod in pods.items if pod.metadata.name in matching_pods}
+    
+    return matching_pods, matching_pod_namespaces
 
 @app.route("/")
 def index():
-    # # Load Kubernetes configuration from default location
+    # Load Kubernetes configuration from default location
+    config.load_incluster_config()
     # config.load_kube_config()
 
-    # Set the namespace from environment variable or use 'default' as default
-    namespace = os.getenv("NAMESPACE", "default")
-
-    # Set the pod status from environment variable or use 'running' as default
+    # Set the pod status from environment variable or use 'Running' as default
     pod_status = os.getenv("POD_STATUS", "Running")
+
+    # Set the pod namespace from environment variable (optional)
+    pod_namespace = os.getenv("POD_NAMESPACE", None)
 
     # Create Kubernetes API client
     api_instance = client.CoreV1Api()
 
-    # Get pods with the specified status
-    matching_pods = get_pods_with_status(api_instance, namespace, pod_status)
+    # Get pods with the specified status and namespace
+    matching_pods, matching_pod_namespaces = get_pods_with_status(api_instance, pod_status, pod_namespace)
 
-    # provides a json response
-    response_data = {"namespace": namespace, "pod_status": pod_status, "matching_pods": matching_pods}
-    return jsonify(response_data)
+    # Render the HTML template
+    return render_template("index.html", matching_pods=matching_pod_namespaces)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
